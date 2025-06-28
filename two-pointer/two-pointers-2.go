340d11259065b514e2b7450f3a66736faee19f76
MADHAVAN S <105551682+madhavan-21@users.noreply.github.com>
2025-06-28 06:38:58 +0000

    Add additional two-pointer algorithm examples and templates

--- two-pointer/two-pointers-2.go
+++ two-pointer/two-pointers-2.go
@@ -32,3 +32,82 @@ func fn(arr1, arr2 []int) int {
 //1 . https://leetcode.com/problems/merge-sorted-array/description/
 //2 . https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
 //3 . https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+
+
+// https://leetcode.com/problems/merge-sorted-array/
+
+// func merge(nums1 []int, m int, nums2 []int, n int)  {
+//     p1 , p2 , p := m-1, n-1 , m+n-1
+
+//     for p1 >= 0 && p2 >= 0{
+//         if nums1[p1] > nums2[p2]{
+//             nums1[p] = nums1[p1]
+//             p1--
+
+//         }else{
+//             nums1[p] = nums2[p2]
+//             p2--
+//         }
+//         p--
+//     }
+
+//     for p2 >= 0{
+//         nums1[p] = nums2[p2]
+//         p2--
+//         p--
+//     }
+// }
+
+//https://leetcode.com/problems/merge-two-sorted-lists/submissions/1678976443/
+
+// func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+// 	dummy := &ListNode{}
+// 	curr := dummy
+
+// 	for l1 != nil && l2 != nil {
+//         if l1.Val < l2.Val{
+//             curr.Next = l1
+//             l1 = l1.Next
+//         }else{
+//             curr.Next = l2
+//             l2 = l2.Next
+//         }
+//         curr = curr.Next
+// 	}
+
+//     if l1 != nil{
+//         curr.Next = l1
+//     }else{
+//         curr.Next = l2
+//     }
+
+//     return dummy.Next
+
+// }
+
+
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/submissions/1678986079/
+
+// func intersect(nums1 []int, nums2 []int) []int {
+//    sort.Ints(nums1)
+// 	sort.Ints(nums2)
+
+//     n1 , n2 := 0 ,0
+
+//     result := []int{}
+
+//     for n1 < len(nums1) && n2 < len(nums2){
+//         if nums1[n1] == nums2[n2]{
+//             result = append(result , nums1[n1])
+//             n1++
+//             n2++
+//         }else if nums1[n1] < nums2[n2]{
+//            n1++
+//         }else{
+//             n2++
+//         }
+//     }
+
+//     return result
+// }
--- two-pointer/two-pointers.go
+++ two-pointer/two-pointers.go
@@ -22,3 +22,94 @@ func twoPointerTemplate(arr []int) int {
 
 //https://leetcode.com/problems/container-with-most-water/description/
 
+//https://leetcode.com/problems/valid-palindrome/submissions/1678915944/
+
+//example
+
+// func isPalindrome(s string) bool {
+// 	left, right := 0, len(s)-1
+
+// 	for left < right {
+// 		for left < right && isWord(rune(s[left])) == false {
+// 			left++
+// 		}
+
+// 		for left < right && isWord(rune(s[right])) == false {
+// 			right--
+// 		}
+
+//         if strings.ToLower(string(s[left])) != strings.ToLower(string(s[right])) {
+//             return false
+//         }
+
+//         left ++
+//         right--
+// 	}
+
+//     return true
+// }
+
+// func isWord(r rune) bool {
+// 	return unicode.IsLetter(r) || unicode.IsDigit(r)
+// }
+
+
+// https://leetcode.com/problems/container-with-most-water/
+
+// func maxArea(height []int) int {
+//     left , right := 0 , len(height)-1
+//     maxArea := 0
+
+//     for left < right{
+//         h := min(height[left],height[right])
+//         width := right -left
+//         area := h * width
+
+//         if area > maxArea{
+//             maxArea = area
+//         }
+
+//         if height[left] < height[right]{
+//             left++
+//         } else{
+//             right --
+//         }
+//     }
+
+//     return maxArea
+
+
+// }
+
+
+// func min(a, b int) int {
+// 	if a < b {
+// 		return a
+// 	}
+// 	return b
+// }
+
+
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+// func twoSum(numbers []int, target int) []int {
+// 	if len(numbers) == 2 {
+// 		return []int{1, 2}
+// 	}
+
+// 	l, r := 0, len(numbers)-1
+
+// 	for l < r {
+//         sum := numbers[l] + numbers[r]
+// 		if sum == target {
+// 			return []int{l + 1, r + 1}
+// 		}else if sum < target{
+//             l++
+//         } else{
+//             r--
+//         }
+// 	}
+
+//     return []int{}
+// }
+
